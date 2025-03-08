6c1ff73765883881bb41cc6e378b30a67c2c2001
a56379375437 <15142876+a56379375437@user.noreply.gitee.com>
2025-03-08 17:28:17 +0800

    Initial commit

---
+++ my-project-backend/src/main/java/com/example/entity/dto/Account.java
@@ -0,0 +1,31 @@
+package com.example.entity.dto;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.example.entity.BaseData;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 数据库中的用户信息
+ */
+@Data
+@TableName("db_account")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account implements BaseData {
+    @TableId(type = IdType.AUTO)
+    Integer id;
+    String username;
+    String password;
+    String email;
+    String role;
+    String avatar;
+    Date registerTime;
+    boolean mute;
+    boolean banned;
+}
